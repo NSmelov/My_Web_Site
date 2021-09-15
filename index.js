cf2a03619fbc2d46159bcf9d1bac87ac6bcdcfbf
NSmelov <nikolajsmelov470@gmail.com>
2021-09-15 21:22:48 +0700

    version 1.0.3

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/gallery', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'Gallery.html'))
+    res.sendFile(path.join(__dirname, 'public', 'gallery.html'))
 })
 
 app.get('/board', (req, res) => {
